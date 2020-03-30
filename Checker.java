d4fea647df8ae1fd094c124cf0ca374c73d7837a
akash.kumar1 <akash.kumar1@oyorooms.com>
2020-03-30 17:58:59 +0530

    method 1 added

--- Checker.java
+++ Checker.java
@@ -1,6 +1,10 @@
 public Class Checker{
 
-	public String printClassName(){
-		return "Checker.java";
-	}
+public String printClassName(){
+return "Checker.java";
+}
+
+public String printMethod1(){
+return "method1"
+}
 }
